db7017c8008f8cad24a71f89e525f24459f826b8
sebastian de-gaetano <sebastiandegaetano@gmail.com>
2022-07-14 10:05:51 -0300

    mod

--- src/main/java/ar/com/codoacodo/dto/Producto.java
+++ src/main/java/ar/com/codoacodo/dto/Producto.java
@@ -1,23 +1,36 @@
 package ar.com.codoacodo.dto;
 
-import java.util.Date;
+import java.util.Date; 
 
 public class Producto {
-	private Long idProducto; 
+	private Long idProducto;
 	private String nombre;
 	private Float precio;
-	private Date fecha; 
+	private Date fecha;
 	private String imagen; 
 	private String codigo;
 	
-	public Producto(Long idProducto, String nombre, Float precio, Date fecha, String imagen, String codigo) {
+//	public Producto(Long idProducto, String nombre, Float precio, Date fecha, String imagen, String codigo) {
+//		super();
+//		this.idProducto = idProducto;
+//		this.nombre = nombre;
+//		this.precio = precio;
+//		this.fecha = fecha;
+//		this.imagen = imagen;
+//		this.codigo = codigo;
+//	}
+
+	public Producto(Long idProducto2, String nombre2, Float precio2, java.util.Date fecha2, String imagen2,
+			String codigo2) {
 		super();
-		this.idProducto = idProducto;
-		this.nombre = nombre;
-		this.precio = precio;
-		this.fecha = fecha;
-		this.imagen = imagen;
-		this.codigo = codigo;
+		this.idProducto = idProducto2;
+		this.nombre = nombre2;
+		this.precio = precio2;
+		this.fecha = fecha2;
+		this.imagen = imagen2;
+		this.codigo = codigo2;
+		
+		// TODO Auto-generated constructor stub
 	}
 
 	public Long getIdProducto() {
@@ -67,5 +80,7 @@ public class Producto {
 	public void setCodigo(String codigo) {
 		this.codigo = codigo;
 	}
+
+	
 	
 }
